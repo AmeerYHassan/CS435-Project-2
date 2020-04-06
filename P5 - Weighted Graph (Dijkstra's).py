d757fd3c7ebb4f14eabe50c432eba2c98767d691
AmeerYHassan <ayh7@njit.edu>
2020-04-06 17:15:13 -0400

    Added Parts 5 and 6

---
+++ P5 - Weighted Graph (Dijkstra's).py
@@ -0,0 +1,93 @@
+import random
+import math
+
+class Node:
+    def __init__(self, nodeVal):
+        self.nodeVal = nodeVal
+        self.neighbors = {}
+    
+    def __repr__(self):
+        return str(self.neighbors)
+
+class WeightedGraph:
+    def __init__(self):
+        self.graphNodes = {}
+    
+    def addNode(self, nodeVal):
+        self.graphNodes[nodeVal] = Node(nodeVal)
+    
+    def addWeightedEdge(self, first, second, edgeWeight):
+        self.graphNodes[first].neighbors[second] = edgeWeight
+    
+    def removeDirectedEdge(self, first, second):
+        self.graphNodes[first].neighbors.pop(second)
+    
+    def findNode(self, nodeVal):
+        return self.graphNodes[nodeVal]
+
+    def getAllNodes(self):
+        return self.graphNodes
+
+def createRandomCompleteWeightedGraph(n):
+    randGraph = WeightedGraph()
+
+    for i in range(n):
+        randGraph.addNode(i)
+    
+    nodeList = [*randGraph.graphNodes]
+    for firstNode in nodeList:
+        for secondNode in nodeList:
+            if (firstNode is not secondNode):
+                randGraph.addWeightedEdge(firstNode, secondNode, random.randint(1, max(10, n/2)))
+    
+    return randGraph
+
+def createLinkedList(n):
+    linkedGraph = WeightedGraph()
+
+    for i in range(n):
+        linkedGraph.addNode(i)
+    
+    for i in range(n-1):
+        linkedGraph.addWeightedEdge(i, i+1, 1)
+    
+    return linkedGraph
+
+# NOTE: Graph only used to find neighbors of a node.
+def dijkstras(start, graph):
+    currNode = graph.findNode(start)
+    currVal = start
+
+    distanceDict = {}
+    distanceDict[currVal] = 0
+
+    visitedNodes = set()
+    # While curr is not null and currVal is in the dictionary
+    while(currVal is not None and currVal in distanceDict):
+        # “Finalize” curr.
+        visitedNodes.add(currVal)
+
+        # Iterate over its neighbors, “relax” each neighbor:
+        for neighbor in [*currNode.neighbors]:
+            # Not finalized Neighbor
+            if (neighbor not in visitedNodes):
+                updatedDistance = distanceDict[currVal] + currNode.neighbors[neighbor]
+                if (neighbor not in distanceDict or updatedDistance < distanceDict[neighbor]):
+                    distanceDict[neighbor] = updatedDistance
+        currVal = minDist(distanceDict, visitedNodes)
+        if (currVal is not None):
+            currNode = graph.findNode(currVal)
+        else:
+            break
+
+    return distanceDict
+
+# Returns the node with smallest distance (Taken from repl.it)
+def minDist(distanceDict, visitedNodes):
+    ans = None
+    m = math.inf
+    for curr in [*distanceDict]:
+        if curr not in visitedNodes and distanceDict[curr] <= m:
+            m = distanceDict[curr]
+            ans = curr
+    return ans
---
+++ P6 - Grid Search.py
@@ -0,0 +1,72 @@
+import random
+
+class GridNode:
+    def __init__(self, nodeVal, xCoord, yCoord):
+        self.nodeVal = nodeVal
+        self.neighbors = set()
+        self.coords = (xCoord, yCoord)
+    
+    def __repr__(self):
+        return(str(self.neighbors))
+
+class GridGraph:
+    def __init__(self):
+        self.nodeList = {}
+
+    def addGridNode(self, x, y, nodeVal):
+        self.nodeList[nodeVal] = GridNode(nodeVal, x, y)
+
+    def addUndirectedEdge(self, first, second):
+        self.nodeList[first].neighbors.add(second)
+        self.nodeList[second].neighbors.add(first)
+
+    def removeUndirectedEdge(self, first, second):
+        self.nodeList[first].neighbors.remove(second)
+        self.nodeList[second].neighbors.remove(first)
+
+    def getAllNodes(self):
+        return self.nodeList
+
+def createRandomGridGraph(n):
+    randomGrid = GridGraph()
+    for i in range(n):
+        for j in range(n):
+            randomGrid.addGridNode(j, i, j+i*n)
+    
+    for i in range(n*n):
+        currNeighbors = getNeighbors(randomGrid.nodeList[i], n)
+        for neighbor in currNeighbors:
+            if(random.randint(0, 1) is 0):
+                randomGrid.addUndirectedEdge(i, neighbor)
+    
+    return randomGrid
+
+def getNeighbors(node, n):
+    if (node.coords[0] is 0):
+        if (node.coords[1] is 0):
+            return ([node.nodeVal+1, node.nodeVal+n])
+        elif (node.coords[1] is n-1):
+            return ([node.nodeVal+1, node.nodeVal-n])
+        else:
+            return ([node.nodeVal+1, node.nodeVal+n, node.nodeVal-n])
+    elif (node.coords[0] is n-1):
+        if (node.coords[1] is 0):
+            return ([node.nodeVal-1, node.nodeVal+n])
+        elif (node.coords[1] is n-1):
+            return ([node.nodeVal-1, node.nodeVal-n])
+        else:
+            return ([node.nodeVal-1, node.nodeVal+n, node.nodeVal-n])
+    elif (node.coords[1] is 0):
+        return ([node.nodeVal-1, node.nodeVal+1, node.nodeVal+n])
+    elif (node.coords[1] is n-1):
+        return ([node.nodeVal-1, node.nodeVal+1, node.nodeVal-n])
+    else:
+        return([node.nodeVal-1, node.nodeVal+1, node.nodeVal+n, node.nodeVal-n])
+
+def astar(sourceNode, destNode):
+    # TODO: Implement astar
+    pass
+
+myGrid = createRandomGridGraph(5)
+print(myGrid.getAllNodes())
+
